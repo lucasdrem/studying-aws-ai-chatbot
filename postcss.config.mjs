87ec20be361d826ebe38c0223251a89f7655a535
lucasdrem <grober39@gmail.com>
2025-01-10 16:04:54 -0300

    fix: style autorprefixer

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,7 @@
-const { join } = require("path");
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {
-      config: join(__dirname, "tailwind.config.js"),
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
